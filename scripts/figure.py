c249f0ce06257e8d2bfc16a73bc60e29e05a15b0
spearneil <yuan.chen@whu.edu.cn>
2025-10-09 14:44:02 +0800

    modify figure.py

--- scripts/figure.py
+++ scripts/figure.py
@@ -3,10 +3,9 @@ import os
 import re
 import shutil
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 
 # global parameters
+log_dir = "./logs/"
 store_path = "/mnt/pmem0/ycsb-store"
 store_size = 16
 key_size, val_size = 8, 8
@@ -14,8 +13,8 @@ records_num = 1000000
 run_duration = 1
 enable_pcm = 0
 
-stores = ["pmemkv", "SlabStore", "Plush", "Viper", "RocksDB"]
-
+# stores = ["pmemkv", "SlabStore", "Plush", "Viper", "RocksDB"]
+stores = ["pmemkv", "SlabStore"]
 colors = ['blue', 'red', 'orange', 'green', 'purple', 'steelblue', 'gray']
 markers = ["o", "d", "s", "^", "v", "X", "P"]
 
@@ -35,37 +34,15 @@ def build_project():
         res = subprocess.run(["cmake", "--build", "./build", "-j 32"])
         print("cmake --build ./build/\n", res.stdout)
 
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
 
 def figure_scalability():
-    log_path = "./scale.log"
-
     threads = [1, 2, 4, 8, 16, 24, 32, 40, 48]
     read_ratios = [100, 75, 50, 25, 0]
     workloads = ["Read-Only", "Read-Heavy", "Balanced", "Write-Heavy", "Write-Only"]
     distributions = ["unif", "zipf"]
-
-    log_contents = {}
-    if os.path.exists(log_path):
-        header_line = ""
-        current_section = ""
-        with open(log_path, "r+") as log:
-            section_head = str(["./build/test/test-ycsb-test", store_path])[:-1]
-            for line in log:
-                if line.startswith(section_head):
-                    if current_section:
-                        log_contents[header_line.strip()] = current_section
-                        current_section = ""
-                    header_line = line
-                current_section = current_section + line
-
-        # deal with the last log section
-        match = re.search(r"run phase.*?throughput:\s*([\d.]+)", current_section)
-        if match:
-            log_contents[header_line.strip()] = current_section
-        else:  # exit for some reason, truncate log file
-            with (open(log_path, "w")) as log:
-                for head, section in log_contents.items():
-                    log.write(section)
 
     fig = plt.figure(figsize=(20, 6))
     row, col = len(distributions), len(read_ratios)
@@ -80,7 +57,19 @@ def figure_scalability():
                     ycsb_test = ["./build/test/test-ycsb-test", store_path, str(store_size),
                                  str(sid), str(nthd), str(records_num), str(key_size), str(val_size),
                                  str(read_ratios[cid]), str(run_duration), str(enable_pcm), str(rid)]
-                    result = log_contents.get(str(ycsb_test))
+                    log_name = ('scale-' + str(store_size) + '-' + str(sid) + '-' + str(nthd) + '-' + str(records_num)
+                                + '-' + str(key_size) + '-' + str(val_size) + '-' + str(read_ratios[cid])
+                                + '-' + str(run_duration) + '-' + str(enable_pcm) + '-' + str(rid) + '.log')
+                    log_path = os.path.join(log_dir, log_name)
+
+                    result = ""
+
+                    if os.path.exists(log_path):  # read existing logs
+                        with open(log_path, "r") as log:
+                            result = log.read()
+                        if not re.search(r"run phase.*?throughput:\s*([\d.]+)", result):
+                            result = ""  # existing logs are broken
+                            remove_path(log_path)
 
                     if not result:
                         result = subprocess.run(ycsb_test, capture_output=True, text=True).stdout
@@ -94,7 +83,7 @@ def figure_scalability():
                 plt.plot(threads, perf, label=stores[sid], marker=markers[sid], color=colors[sid],
                          linewidth=2, markersize=10, markeredgewidth=0.4, markeredgecolor='black', alpha=0.95)
             plt.xticks(threads[1:])
-            plt.xlim(threads[0], threads[-1] + 1)
+            plt.xlim(0, threads[-1] + 1)
             plt.grid(axis='y', color='darkgray', linestyle=':', linewidth=2, alpha=0.4)
 
     fig.tight_layout()
